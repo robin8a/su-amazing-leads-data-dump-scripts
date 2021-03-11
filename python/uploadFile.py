35e4ad9ce528e9a233df6d0fbf1b2c1c90022831
ROBIN OCHOA <robin8a@local.mac>
2021-03-10 23:58:16 -0500

    update

--- python/uploadFile.py
+++ python/uploadFile.py
@@ -1,18 +1,21 @@
 from google.cloud import storage
-
-def upload_to_bucket(blob_name, path_to_file, bucket_name):
-    """ Upload data to a bucket"""
-
-    # Explicitly use service account credentials by specifying the private key
-    # file.
-    storage_client = storage.Client.from_service_account_json(
-        'creds.json')
-
-    #print(buckets = list(storage_client.list_buckets())
-
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(blob_name)
-    blob.upload_from_filename(path_to_file)
-
-    #returns a public url
-    return blob.public_url
+3
+4
+# Setting credentials using the downloaded JSON file
+5
+client = storage.Client.from_service_account_json(json_credentials_path='credentials-python-storage.json')
+6
+7
+# Creating bucket object
+8
+bucket = client.get_bucket('py-python')
+9
+10
+# Name of the object to be stored in the bucket
+11
+object_name_in_gcs_bucket = bucket.blob('my_first_gcs_upload.png')
+12
+13
+# Name of the object in local file system
+14
+object_name_in_gcs_bucket.upload_from_filename('gcs.png')
