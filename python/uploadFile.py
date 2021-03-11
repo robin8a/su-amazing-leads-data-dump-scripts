b36dee15586e9e27c5496f235d41e53d755b6400
ROBIN OCHOA <robin8a@local.mac>
2021-03-11 00:00:59 -0500

    update

--- python/uploadFile.py
+++ python/uploadFile.py
@@ -3,7 +3,7 @@ from google.cloud import storage
 4
 # Setting credentials using the downloaded JSON file
 5
-client = storage.Client.from_service_account_json(json_credentials_path='credentials-python-storage.json')
+client = storage.Client.from_service_account_json(json_credentials_path='kio-sl-emap-4efaa41838cd.json')
 6
 7
 # Creating bucket object
