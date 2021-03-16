b251ac79e4466b9efafb211631f82da0189a36cd
ROBIN OCHOA <robin8a@local.mac>
2021-03-16 11:26:14 -0500

    Interaction data dump

--- crontab/data_dump.sh
+++ crontab/data_dump.sh
@@ -69,6 +69,20 @@ echo "$(date +"%m-%d-%Y %H:%M:%S"): Conveting tsv to csv table: QuestionaryInter
 tr '\t' ',' < "$data_dump_path$data_dump_today_date/"QuestionaryInteractionDataDump_$data_dump_today_date.tsv > "$data_dump_path$data_dump_today_date/"QuestionaryInteractionDataDump_$data_dump_today_date.csv
 echo "$(date +"%m-%d-%Y %H:%M:%S"): Converted to csv table: QuestionaryInteraction" >> $REC_LOG_FILE
 
+## Interaction
+aws dynamodb scan \
+    --table-name Interaction-mexfa73ymfc6rmlwqmt6vu4vf4-suamaleapi \
+    --query "Items[*].[id.S, __typename.S, countOutside.N, createdAt.S, distance.N, distance_left_button_point.N, distance_questionary_point.N, distance_right_button_point.N, dt.N, element.S, epoch.N, height.N, isActive.BOOL, isMouseDetected.BOOL, isPositionOutside.BOOL, isTouchDetected.BOOL, questionID.S, questionaryInteractionID.S, speed.N, speedAverage.N, sumDistance.N, sumTimeMiliseconds.N, sumTimeMilisecondsBeforeNextQuestion.N, type.S, updatedAt.S, width.N, x.N, y.N]" \
+    --filter-expression 'begins_with(createdAt, :val)' \
+    --expression-attribute-values '{":val": {"S": "'$data_dump_today_date'"}}' \
+    --output text > "$data_dump_path$data_dump_today_date/"InteractionDataDump_$data_dump_today_date.tsv
+
+echo "$(date +"%m-%d-%Y %H:%M:%S"): Interaction table Data Dumped ..." >> $REC_LOG_FILE
+
+echo "$(date +"%m-%d-%Y %H:%M:%S"): Conveting tsv to csv table: Interaction ..." >> $REC_LOG_FILE
+tr '\t' ',' < "$data_dump_path$data_dump_today_date/"InteractionDataDump_$data_dump_today_date.tsv > "$data_dump_path$data_dump_today_date/"InteractionDataDump_$data_dump_today_date.csv
+echo "$(date +"%m-%d-%Y %H:%M:%S"): Converted to csv table: Interaction" >> $REC_LOG_FILE
+
 
 echo "$(date +"%m-%d-%Y %H:%M:%S"): #####" >> $REC_LOG_FILE
 
