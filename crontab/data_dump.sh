094142ebe46aaea21423e8d1ec5b00ca4542a258
ROBIN OCHOA <robin8a@local.mac>
2021-03-16 11:07:26 -0500

    tsv to csv

--- crontab/data_dump.sh
+++ crontab/data_dump.sh
@@ -34,14 +34,13 @@ aws dynamodb scan \
     --expression-attribute-values '{":val": {"S": "'$data_dump_today_date'"}}' \
     --output text > "$data_dump_path$data_dump_today_date/"QuestionAnswerDataDump_$data_dump_today_date.tsv
 
-# aws dynamodb scan \
-#     --table-name QuestionAnswer-mexfa73ymfc6rmlwqmt6vu4vf4-suamaleapi \
-#     --query "Items[*].[id.S, __typename.S, createdAt.S, optionID.S, questionID.S, questionaryInteractionID.S, updatedAt.S]" \
-#     --filter-expression 'begins_with(createdAt, :val)' \
-#     --expression-attribute-values '{":val": {"S": "2021-03-16"}}' \
-#     --output text > "$data_dump_path$data_dump_today_date/"QuestionAnswerDataDump_test.tsv
-
 echo "$(date +"%m-%d-%Y %H:%M:%S"): QuestionAnswer table Data Dumped ..." >> $REC_LOG_FILE
+
+echo "$(date +"%m-%d-%Y %H:%M:%S"): Conveting tsv to csv table: QuestionAnswer ..." >> $REC_LOG_FILE
+tr '\t' ',' < "$data_dump_path$data_dump_today_date/"QuestionAnswerDataDump_$data_dump_today_date.tsv > "$data_dump_path$data_dump_today_date/"QuestionAnswerDataDump_$data_dump_today_date.csv
+echo "$(date +"%m-%d-%Y %H:%M:%S"): Converted to csv table: QuestionAnswer >> $REC_LOG_FILE
+
+
 
 echo "$(date +"%m-%d-%Y %H:%M:%S"): #####" >> $REC_LOG_FILE
 
