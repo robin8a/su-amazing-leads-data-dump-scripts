92e774e62f1a36c6e811285f96f1c085e70c0c40
ROBIN OCHOA <robin8a@local.mac>
2021-04-02 10:24:21 -0400

    ready to test erase folde n days

--- crontab/erase_folder_n_days.sh
+++ crontab/erase_folder_n_days.sh
@@ -4,4 +4,4 @@
 . /home/ec2-user/su-amazing-leads-data-dump-scripts/conf/conf.properties
 # sudo find . /home/ec2-user/su-amazing-leads-data-dump-scripts/data/* -type d -ctime +3
 # sudo find . /home/ec2-user/su-amazing-leads-data-dump-scripts/data/* -type d -ctime +3 | sudo xargs rm -rf
-sudo find . /home/ec2-user/su-amazing-leads-data-dump-scripts/data/* -type d -ctime +$conf_numbers_days_store | sudo xargs rm -rf
+sudo find . /home/ec2-user/su-amazing-leads-data-dump-scripts/data_dump/* -type d -ctime +$conf_numbers_days_store | sudo xargs rm -rf
