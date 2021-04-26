486c98c6bff8771297a31e38c8bc3742eb6ffa46
ROBIN OCHOA <robin8a@local.mac>
2021-04-26 07:28:43 -0500

    crontab

--- 000_crontab.sh
+++ 000_crontab.sh
@@ -1,6 +1,6 @@
 # m h  dom mon dow   command
-# */1 * * * *   /home/pi/Scripts/Startrecording-m3u8.sh
-# execute at 2am daily, delete folder older than conf.properites /home/ec2-user/su-amazing-leads-data-dump-scripts/
+# execute at 23:00 daily, delete folder older than conf.properites /home/ec2-user/su-amazing-leads-data-dump-scripts/data_dump/
 0 23 * * * /home/ec2-user/su-amazing-leads-data-dump-scripts/crontab/erase_folder_n_days.sh
+# execute at 23:00 daily, data dump dynamo db on /home/ec2-user/su-amazing-leads-data-dump-scripts/data_dump/
 0 23 * * * /home/ec2-user/su-amazing-leads-data-dump-scripts/crontab/data_dump.sh
 
